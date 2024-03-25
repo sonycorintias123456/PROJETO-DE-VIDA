eb98621c1c6ed3eeb6b14f9a1a3035da3456f797
sonycorintias123456 <106813699+sonycorintias123456@users.noreply.github.com>
2024-03-25 11:47:48 -0300

    .

--- main.js
+++ main.js
@@ -12,3 +12,8 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
